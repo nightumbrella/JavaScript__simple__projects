aa0d65bc5691f83a6ae4e13637b4a3344aab96cb
NightUmbrella <>
2023-02-19 03:44:31 +0500

    API not working nap time 3:44AM

---
+++ src/main.js
@@ -0,0 +1,41 @@
+
+
+// const APIURL = "https://api.github.com/users/"
+// const form = document.getElementById("form")
+// const search = document.getElementById("search")
+
+// // getUser("bradtraversy")
+
+// async function getUser(username){
+//     try{
+//         const {data} = await axios(APIURL + username)
+//         console.log(data)
+//     } catch(err){
+//         console.log(err);
+//     }
+// }
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault()
+    
+//     const user = search.value
+
+
+//     if(user){
+//         getUser(user)
+//         search.value = ""
+//     }
+// })
+
+
+
+// const APIURL = 'https://api.github.com/users'
+
+// getUser("plorinpop17")
+
+// async function getUser(username){
+//     const res = await axios(APIURL + username)
+
+//     console.log(res.data)
+// }
+
